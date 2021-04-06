d2fc87f90adf7ecd9905c8cd3db135f40cf9c943
codychan5 <77872727+codychan5@users.noreply.github.com>
2021-04-06 13:09:18 +0800

    Add files via upload

---
+++ Data Frame_Toronto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd 
+
+
+# In[11]:
+
+
+data = pd.read_csv(r"C:\Users\samsung\Desktop\Geospatial_Coordinates.csv")
+
+
+# In[12]:
+
+
+data.head()
+
+
+# In[ ]:
+
+
+
+
